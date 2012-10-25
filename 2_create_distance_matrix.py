75eea03f98a0a4bf496c44869d15438648e8f738
Rohit <rejarohit2004@gmail.com>
2012-10-25 13:11:54 -0400

    updated

--- 2_create_distance_matrix.py
+++ 2_create_distance_matrix.py
@@ -7,6 +7,16 @@ from random import randint
 def process_files(idxdir,options):
     count = 1
     idxData = {}
+    encData = {}
+    allData = {}
+    filehash = {}
+    taglist = []
+    enriched = open(options.enrcfile,"r")
+    for line in enriched:
+        chrom,junk,junk,start,end,junk,junk,junk,junk  = line.rstrip().split("\t")
+        encData[chrom+":"+start+":"+end] = 1
+    
+    # Reading all idx files in the directory one by one
     for fname in os.listdir(idxdir):
         if fname.endswith("idx") or fname.endswith("tab"):
             input = open(os.path.join(idxdir,fname),"r")
@@ -14,27 +24,34 @@ def process_files(idxdir,options):
                 if line.startswith("chrom") or line.startswith("#"):
                     continue;
                 chrom, start,ftag,rtag,ttag = line.rstrip().split("\t")
-                idxData[str(count)+"\t"+chrom+"\t"+start] = int(ttag)
-            count = count+1
-    print "Done reading all files in memory"
-    rand_num = randint(1,5000)
-    mainline = return_random_line_from_file(options,rand_num).split("\t")
-    print mainline
-    readfile = open(options.enrcfile,"r")
-    for line in readfile:
-        count = 1
-        if count == rand_num:
-            continue
-        chrom, junk,junk,start,end,junk,junk,junk,junk = line.split("\t")
+                idxData[chrom+"\t"+start] = int(ttag)
                 
-        
-def return_random_line_from_file(options,rand_num):
-    count = 1
-    input = open(options.enrcfile,"r")
-    for line in input:
-        if count == rand_num:
-            return(line)
+        # Calculating the tags present in the enrcihed regions.
+        for key, val in encData.items():
+            l = key.split(":")
+            for i in range(int(l[1]),int(l[2])):         
+                idx = l[0]+"\t"+str(i)
+                if idx in idxData:
+                    taglist.append(idxData[idx])
+                else:
+                    taglist.append(0)
+                    
+            allData[str(count)+":"+key] = taglist
+            taglist = []
+        filehash[count] = fname
         count = count+1
+    
+    create_matrix_for_regions(allData)               
+
+def create_matrix_for_regions(allData):
+    for majorkey, majorval in allData.items():
+        for minorkey, minorval in allData.items():
+            compute_distance(majorkey,minorkey)
+            
+                        
+def print_dict(dictionary):
+    for key, val in dictionary.items():
+        print key,val
 
 
 usage = '''
@@ -55,8 +72,8 @@ def run():
     parser = OptionParser(usage='%prog [options] input_paths', description=usage, formatter=CustomHelpFormatter())
     parser.add_option('-e', action='store',  dest='enrcfile',
                       help='File containing the enriched regions.')
-    parser.add_option('-w', action='store', type='int', dest='window',default=200,
-                      help='Size of the moving window.Default=200')
+    parser.add_option('-w', action='store', type='int', dest='window',default=100,
+                      help='Size of the moving window.Default=100')
     parser.add_option('-s', action='store', type='int', dest='stp_size',default=1,
                       help='Slide the window with given bp length in an interval.Defalut=1')
     (options, args) = parser.parse_args()
