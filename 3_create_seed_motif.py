c19b9d630607f105c550470b5038d39d176cb2fb
Rohit <rejarohit2004@gmail.com>
2012-11-24 15:33:05 -0500

    testing rand

--- 3_create_seed_motif.py
+++ 3_create_seed_motif.py
@@ -1,9 +1,19 @@
 import sys, os, operator, random, time
 from optparse import OptionParser , IndentedHelpFormatter
 
+def process_files(infile,options):
+    randList = generate_random_numbers(options.sample,options.max)
+    input = open(infile,"rt")
+    
 
 
-
+def generate_random_numbers(s,m):
+    randlist = []
+    for i in range(1,s):
+        j = random.randint(1,m)
+        if j not in randlist:
+            randlist.append(j)
+    return(randlist)
 
 
 
@@ -24,8 +34,10 @@ class CustomHelpFormatter(IndentedHelpFormatter):
   
 def run():
     parser = OptionParser(usage='%prog [options] input_paths', description=usage, formatter=CustomHelpFormatter())
-    #parser.add_option('-d', action='store',  dest='distFile',
-    #                  help='File containing the distance matrix.')
+    parser.add_option('-n', action='store', type='int', dest='sample',default = 100,
+                      help='The number of rows to sample at a time.')
+    parser.add_option('-m', action='store', type='int', dest='max',default = 5000,
+                      help='Generate random nummbers between 1 and max.')
     parser.add_option('-o', action='store', dest='offFile',
                       help='File containing the offset matrix')
     (options, args) = parser.parse_args()
