3b8a8c8138087f0f7d33a04c101e087710600a54
Rohit Reja <rzr142@lionxi.rcc.psu.edu>
2012-11-27 13:58:14 -0500

    updated

--- 3_create_seed_motif.py
+++ 3_create_seed_motif.py
@@ -7,7 +7,7 @@ def process_files(infile,options):
         Omatx = {}
         count = 0
         randList = generate_random_numbers(options.sample,options.max)
-        #print randList
+        print randList
         # First reading the distance matrix file and sampling 100 rows.
         input1 = open(infile,"rt")
         for line in input1:
@@ -36,20 +36,54 @@ def process_files(infile,options):
                 break
         # Now for each of the 100 sampled rows, find the top 20 distances and their corresponding offsets.    
         for key,val in Dmatx.items():
-            print key
-            sorted_val = sorted(val.iteritems(), key=operator.itemgetter(1),reverse=True)
-            # Getting offset for the same key.
-            offsets = get_offsets(Omatx[key],sorted_val[:20])
-            for k,v in offsets.items():
-                print k,v
+            #print key
+            sorted_val = sorted(val.iteritems(), key=operator.itemgetter(1),reverse=False)
+            ## Getting offset for the same key.
+            ## Taking top 100 closest loci and looking which "key" window was found overlapping the most.
+            offsets = get_offsets(Omatx[key],sorted_val[:100]) # Change the number here to change the 'top x' selection.
+            encWindows = find_most_enriched_window(offsets)
+            # iterate through all the enriched windows and create average profile
+            if not encWindows == 0:
+                for k,v in encWindows.items():
+                    # Get the coordinates for actual window in that locus.
+                    k = get_actual_cord(k,(v.split(":"))[1],options)
+            else:
+                continue
             sys.exit(1)
-            #print sorted_val[:20]
-            # Run this command from CRM_discovey folder: python 3_create_seed_motif.py -m 10 -n 7 -o testdata/output/tmpoffset.txt testdata/output/tmpdist.txt
-            # Create a function and pass these arguments.(key,offsets)
+            ##print sorted_val[:20]
+            ## Run this command from CRM_discovey folder: python 3_create_seed_motif.py -m 10 -n 7 -o testdata/output/tmpoffset.txt testdata/output/tmpdist.txt
+            ## Create a function and pass these arguments.(key,offsets)
     
         sys.exit(1)
                 
-                
+def find_most_enriched_window(offsets):
+    tmpdict = {}
+    returndict = {}
+    for k,v in offsets.items():
+        tmp = v.split(":")
+        if tmp[0] in tmpdict:
+            tmpdict[tmp[0]] = tmpdict[tmp[0]]+1
+        else:
+            tmpdict[tmp[0]] = 1
+    sorted_tmpdict = sorted(tmpdict.iteritems(), key=operator.itemgetter(1),reverse=True)
+    top_window =  int(sorted_tmpdict[0][0])
+    # From the dictionary sepeate the ones containing the top window and put it in returndict
+    for k,v in offsets.items():
+        tmp = v.split(":")
+        if int(tmp[0]) == top_window:
+            returndict[k] = v
+    # So return the dictionary when you find atleast 20 enriched windows among 100.
+    if len(returndict.keys()) < 20:
+        return(0)
+    else:
+        return(returndict)
+            
+def get_actual_cord(key,windowNo,options):
+    tmp = key.split(":")
+    start = int(tmp[1]) + options.bins*windowNo
+    end = start + options.windowLength
+    return(tmp[0]+":"+str(start)+":"+str(end))
+         
 def get_offsets(dicti,tuple1):
     offsetDict = {}
     list1 = []
@@ -59,9 +93,7 @@ def get_offsets(dicti,tuple1):
         if k in list1:
             offsetDict[k] = v
     return(offsetDict)
-        
-        
-    
+              
 def map_list_to_list(list1,list2,param):
     dicti = {}
     if param == "dist":
@@ -72,8 +104,6 @@ def map_list_to_list(list1,list2,param):
             dicti[list2[i]] = list1[i]
     return(dicti)
         
-
-
 def generate_random_numbers(s,m):
     randlist = []
     randlist = random.sample(range(2,m), s)
@@ -104,6 +134,10 @@ def run():
                       help='File containing the offset matrix')
     parser.add_option('-s', action='store', type='int', dest='nseed',default = 50,
                       help='The number of seed motif to generate.')
+    parser.add_option('-w', action='store', type='int', dest='windowLength',default=100,
+                      help='Size of the moving window.Should be same as the one used in previous step.Default=100')
+    parser.add_option('-b', action='store', type='int', dest='bins',default=5,
+                      help='Sub bin within a window. Should be same as the one used in previous step. This will also be used as a sliding distance.Default=5')
     (options, args) = parser.parse_args()
     
     # Check if all the required arguments are provided, else exit     
