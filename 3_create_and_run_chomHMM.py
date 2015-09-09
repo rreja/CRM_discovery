e50d53380334c234b54281af99eb5c77ac3a9ea9
Rohit <rejarohit2004@gmail.com>
2015-09-09 15:24:53 -0400

    updated 3_create..

--- 3_create_and_run_chomHMM.py
+++ 3_create_and_run_chomHMM.py
@@ -4,50 +4,66 @@ from collections import defaultdict
 import numpy as np
 
 
-def process_file(options):
+def process_file(options,outdir,hmmdir):
     
-    order = []
-    peak_data = defaultdict(list)
-    # Reading the peak-pair directory
-    for fname in os.listdir(options.dir):
-        if not fname.endswith(".gff"):
-            continue
-        label = fname.split("_")[0]
-        order.append(label)
-        in0 = open(os.path.join(options.dir,fname),"rt")
-        for line in in0:
-            if line.startswith("#") or line.startswith("chrom"):
-                continue
-            cols = line.rstrip().split("\t")
-            peak_data[label+":"+cols[0]].append(cols[3]+":"+cols[4])
-        
-        in0.close()
+    #order = []
+    #peak_data = defaultdict(list)
+    ## Reading the peak-pair directory
+    #for fname in os.listdir(options.dir):
+    #    if not fname.endswith(".gff"):
+    #        continue
+    #    label = fname.split("_")[0]
+    #    order.append(label)
+    #    in0 = open(os.path.join(options.dir,fname),"rt")
+    #    for line in in0:
+    #        if line.startswith("#") or line.startswith("chrom"):
+    #            continue
+    #        cols = line.rstrip().split("\t")
+    #        peak_data[label+":"+cols[0]].append(cols[3]+":"+cols[4])
+    #    
+    #    in0.close()
+    #
+    #        
+    ## Read the chromosome length file
+    #in0 = open(options.ref,"rt")
+    #for line in in0:
+    #    cols = line.rstrip().split("\t")
+    #    chrom = cols[0]
+    #    start = 1
+    #    end = int(cols[1])
+    #    outfile = os.path.join(outdir,chrom+"_binary.txt")
+    #    out = open(outfile,"w")
+    #    out.write("BY4741\t"+chrom+"\n")
+    #    out.write("\t".join(order)+"\n")
+    #    
+    #    print "Processning chromosome = "+chrom
+    #    for j in range(start,end,options.ilen):
+    #        int_start = j
+    #        int_end = j + options.ilen - 1
+    #        if int_end > end:
+    #            continue
+    #        out.write(get_value(order,peak_data,chrom,int_start,int_end)+"\n")
+    #    
+    #    out.close()
+    #        
+    #print "Completed creating input!"
+
+
+    ## Run ChromHMM
+    # Edit file paths to accomodate blank spaces in file name.
+    chromHMM = os.path.join(os.path.dirname(os.path.realpath(__file__)),"ChromHMM")
+    chromHMM_command = "'"+chromHMM+"/ChromHMM.jar'"
+    outdir_modified = "'"+outdir+"'"
+    hmmdir_modified = "'"+hmmdir+"'"
     
-            
-    # Read the chromosome length file
-    in0 = open(options.ref,"rt")
-    for line in in0:
-        cols = line.rstrip().split("\t")
-        chrom = cols[0]
-        start = 1
-        end = int(cols[1])
-        outfile = os.path.join(options.dir,chrom+"_binary.txt")
-        out = open(outfile,"w")
-        out.write("BY4741\t"+chrom+"\n")
-        out.write("\t".join(order)+"\n")
-        
-        print "Processning chromosome = "+chrom
-        for j in range(start,end,options.ilen):
-            int_start = j
-            int_end = j + options.ilen - 1
-            if int_end > end:
-                continue
-            out.write(get_value(order,peak_data,chrom,int_start,int_end)+"\n")
-        
-        out.close()
-            
-    print "Complete!"
-        
+    #print "java -mx1600M -jar "+chromHMM_command+" LearnModel "+outdir+" "+hmmdir+" "+str(options.state)+" sg11"
+    os.system("java -mx1600M -jar "+chromHMM_command+" LearnModel "+outdir_modified+" "+hmmdir_modified+" "+str(options.state)+" sg11")
+    
+    print "Completed Running chromHMM, output in "+hmmdir
+    
+
+
+
 def get_value(order,peak_data,chrom,start,end):
     
     binary_values = []
@@ -103,15 +119,21 @@ def run():
                       help='Directory containing the peak-pair files.')
     parser.add_option('-l', action='store', type='int', dest='ilen', default = 200,
                       help='Length of interval')
-    #parser.add_option('-m', action='store', type='int', dest='col1', 
-    #                  help='column of the unique identifier in the Input list file')
+    parser.add_option('-s', action='store', type='int', dest='state',default = 12, 
+                      help='Number of states, default = 10')
     
     
     (options, args) = parser.parse_args()
-    #outdir = os.path.dirname(options.inputlist)
+    # output dir for ChromHM input
+    outdir = os.path.join(options.dir,"_chromHMM_INPUT")
+    if not os.path.exists(outdir): os.makedirs(outdir)
+    
+    # output dir for ChromHMM output
+    hmmdir = os.path.join(options.dir,"_chromHMM_OUTPUT")
+    if not os.path.exists(hmmdir): os.makedirs(hmmdir)
     
  
-    process_file(options)
+    process_file(options,outdir,hmmdir)
     
     
     
