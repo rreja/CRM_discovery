8b9df909ea9776f2fc99f9719c70488c7f162341
Rohit <rejarohit2004@gmail.com>
2015-09-23 08:45:02 -0400

    mapping script added

--- 8_map_tags_to_ref.py
+++ 8_map_tags_to_ref.py
@@ -27,43 +27,103 @@ def process_file(options):
         
         # Get information about the motif
         Tss_file = os.path.join(direc_path,"enriched_TSS.gff")
+        bam_labs = []
+        for bam_labels in open(os.path.join(direc_path,"factors.txt"),"rt"):
+            bam_labs.append(bam_labels.rstrip())
         
         for fname in os.listdir(os.path.join(direc_path,"_candidate_refs")):
             if not fname.endswith(".gff"):
                 continue
-            label = fname.split("-")[0]
+            ref_label = fname.split("-")[0]
             # Create directory for motifs
             
             #Create dicrecotories for CDT files.
-            outdir = os.path.join(direc_path,"_CDT_"+label)
+            outdir = os.path.join(direc_path,"_CDT_"+ref_label)
             if not os.path.exists(outdir): os.makedirs(outdir)
             
             ref_file = os.path.join(os.path.join(direc_path,"_candidate_refs"),fname)
-            bam_file = bams[label]
-            samfile = pysam.AlignmentFile(bam_file, "rb")
-            cdt_file = {}
-            in0 = open(ref_file,"rt")
             
-            for line in in0:
-                if line.startswith("chrom") or line.startswith("#"):
-                    continue
-                cols = line.rstrip().split("\t")
-                chrom = cols[0]
+            for label in bam_labs:
+                bam_file = bams[label]
+                samfile = pysam.AlignmentFile(bam_file, "rb")
+                cdt_file = {}
                 
-                if cols[6] == "+":
-                    start = int(cols[3]) - options.up
-                    end = int(cols[3]) + options.down
-                else:
-                    start = int(cols[4]) - options.down
-                    end = int(cols[4]) + options.up
-                
-                key = cols[0]+":"+str(start)+":"+str(end)+":"+cols[6]
-                cdt_file[key] = get_tags_in_interval(samfile,chrom,start,end)
-                
-            in0.close()
-            write_sense_antisense_file(cdt_file,outdir,label,options)
+                in0 = open(ref_file,"rt")
+                for line in in0:
+                    if line.startswith("chrom") or line.startswith("#"):
+                        continue
+                    cols = line.rstrip().split("\t")
+                    chrom = cols[0]
+                    
+                    if cols[6] == "+":
+                        ## When working with sense and anti-sense tags uncomment these.
+                        #start = int(cols[3]) - options.up
+                        #end = int(cols[3]) + options.down
+                        ## When working with shifted tags: to include the coordinates that might be in the interval after the shift.
+                        start = int(cols[3]) - options.up - options.shift
+                        end = int(cols[3]) + options.down + options.shift
+                    else:
+                        ## When working with sense and anti-sense tags uncomment these.
+                        #start = int(cols[4]) - options.down
+                        #end = int(cols[4]) + options.up
+                        ## When working with shifted tags: 
+                        start = int(cols[4]) - options.down - options.shift
+                        end = int(cols[4]) + options.up + options.shift
+                    
+                    key = cols[0]+":"+str(start)+":"+str(end)+":"+cols[6]
+                    cdt_file[key] = get_tags_in_interval(samfile,chrom,start,end)
+                    
+                in0.close()
+                write_shifted_tags(cdt_file,outdir,label,options)
+                #write_sense_antisense_file(cdt_file,outdir,label,options)
+        sys.exit(1)
     
+
+def write_shifted_tags(cdt_file,outdir,label,options):
+    
+    out_shifted = open(os.path.join(outdir,label+"_shifted_tags.cdt"),"w")
+    cdt_file = return_shifted(cdt_file)
+    write_header(out_shifted,options)
+    for k,v in cdt_file.items():
+        chrom = k.split(":")[0]
+        start = int(k.split(":")[1]) + options.shift
+        end = int(k.split(":")[2]) - options.shift
+        strand = k.split(":")[3]
+        sense = []
+        anti = []
+        
+        # Create sense and anti lists to store the cooridnates at each position
+        for j in range(start,end+1):
+            if strand == "+":
+                key = chrom+":"+str(j)
+                if key in v:
+                    sense.append(v[key])
+                else:
+                    sense.append(0)
+                    
+            elif strand == "-":
+                key = chrom+":"+str(j)
+                if key in v:
+                    anti.append(v[key])
+                else:
+                    anti.append(0)
+                
+                    
+        # Write the sense list as it is but for TSS on antisense strand flip the coordinates.
+        if strand == "+":
+            line = k+"\t1"
+            for j in sense:
+                line = line+"\t"+str(j)
+            out_shifted.write(line+"\n")
             
+        elif strand == "-":
+            line = k+"\t1"
+            for j in reversed(anti):
+                line = line+"\t"+str(j)
+            out_shifted.write(line+"\n")
+            
+    out_shifted.close()
+    
         
 def write_sense_antisense_file(cdt_file,outdir,label,options):
     out_sense = open(os.path.join(outdir,label+"_sense.cdt"),"w")
@@ -132,7 +192,30 @@ def write_sense_antisense_file(cdt_file,outdir,label,options):
     out_sense.close()
     out_anti.close()
 
-
+def return_shifted(cdt_file):
+    shifted_cdt = {}
+    for k,v in cdt_file.items():
+        shifts = {}
+        for key,vals in v.items():
+            chrom = key.split(":")[0]
+            idx = int(key.split(":")[1])
+            strand = key.split(":")[2]
+            if strand == "+":
+                new_idx = idx + 6
+            elif strand == "-":
+                new_idx = idx - 6
+            if new_idx <= 0:
+                continue
+            if chrom+":"+str(new_idx) in shifts:
+                shifts[chrom+":"+str(new_idx)] = shifts[chrom+":"+str(new_idx)] + vals
+            else:
+                shifts[chrom+":"+str(new_idx)] = vals
+                
+        shifted_cdt[k] = shifts
+    
+    return(shifted_cdt)
+            
+            
 def get_tags_in_interval(samfile,chrom,start,end):
     data = {}
     for read in samfile.fetch(chrom,start,end):
@@ -170,6 +253,7 @@ def get_tags_in_interval(samfile,chrom,start,end):
             
             
         else:
+            
             if read.is_unmapped:
                continue
              # Find out if the read is mapped to the sense strand or the reverse to get the 5p end
@@ -192,9 +276,7 @@ def get_tags_in_interval(samfile,chrom,start,end):
 
     return(data)
         
-        
-        
-            
+                   
 def  write_header(out,options):
     line = "Uniqe ID\tGWEIGHT"
     for j in range(-1*(options.up),options.down+1):
@@ -232,6 +314,8 @@ def run():
                       help='Upstream distance from ref = 500.')
     parser.add_option('-d', action='store', type='int', dest='down',default = 500,
                       help='Downstream distance from ref = 500.')
+    parser.add_option('-s', action='store', type='int', dest='shift',default = 6,
+                      help='Shift tags in the 3-prime direction before plotting, default = 6')
     
     
     (options, args) = parser.parse_args()
